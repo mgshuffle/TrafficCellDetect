da8b7d72eac5fa2e7fc43865d17a7015f6a3599e
mgshuffle <mgshuffle@gmail.com>
2016-09-15 21:25:21 +0800

    try1

--- SDCalibrate.m
+++ SDCalibrate.m
@@ -1,7 +1,7 @@
 %% FDCalibrate: 10s dynamic speed-density relation built on reconstructed data (sampling frequency=1Hz)
 function [a, b] = SDCalibrate(aggLoopData)
 	%do sth new
-
+	%This is done by Branch "trying"
 	a = 0;
 	b = 0;
 end
