c4021b5a665cee9b6c8f762f7f55acdd5ce26296
mgshuffle <mgshuffle@gmail.com>
2016-09-15 21:52:44 +0800

    Revert "trash"
    This reverts commit 30c48e4ced84bc1766250a6066f33d264b9859d5.

--- SDCalibrate.m
+++ SDCalibrate.m
@@ -1,6 +1,6 @@
 %% FDCalibrate: 10s dynamic speed-density relation built on reconstructed data (sampling frequency=1Hz)
 function [a, b] = SDCalibrate(aggLoopData)
-	%%local trash no change in remote version
+	
 	a = 0;
 	b = 0;
 end
