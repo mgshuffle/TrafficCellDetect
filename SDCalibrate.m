e342b90013f3610b5dfffdfc619a06b8094da994
mgshuffle <mgshuffle@gmail.com>
2016-09-15 20:28:01 +0800

    SDC changed
    testing

--- SDCalibrate.m
+++ SDCalibrate.m
@@ -1,6 +1,6 @@
 %% FDCalibrate: 10s dynamic speed-density relation built on reconstructed data (sampling frequency=1Hz)
 function [a, b] = SDCalibrate(aggLoopData)
-	
+	%do sth new
 
 	a = 0;
 	b = 0;
